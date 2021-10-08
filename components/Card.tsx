3b0288a1673854e3d1583486ac2bcba7b665eea5
Andrew Stephan <thekiteeatingtree@gmail.com>
2021-10-08 10:13:05 -0400

    add some spacing on the sides of the tables

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,9 +7,11 @@ interface Props {
 
 export function Card({ title, children }: Props) {
     return (
-        <div className="w-full text-indigo-600 rounded-xl bg-gradient-to-r from-blue-50 to-indigo-100 p-10">
-            <Title>{title}</Title>
-            {children}
+        <div className="w-full px-5">
+            <div className="w-full text-indigo-600 rounded-xl bg-gradient-to-r from-blue-50 to-indigo-100 p-10">
+                <Title>{title}</Title>
+                {children}
+            </div>
         </div>
     );
 }
